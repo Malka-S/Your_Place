a2193649ae21399a3128d2642cab2ebdf012cd5c
Sara89850 <saraeliyahu2017@gmail.com>
2020-09-24 18:10:45 +0300

    System.Web.Http.Cors

--- API/App_Start/WebApiConfig.cs
+++ API/App_Start/WebApiConfig.cs
@@ -1,24 +1,41 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace API
 {
-    public static class WebApiConfig
+    public static class WebApiConfig 
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
 
-            // Web API routes
+            var cors = new EnableCorsAttribute("http://localhost:44390", "*", "*");
+            config.EnableCors(cors);
             config.MapHttpAttributeRoutes();
-
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+            var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            {//// Web API configuration and services
+             //var cors = new EnableCorsAttribute("https://localhost:44390", "*", "*");
+             //config.EnableCors(cors);
+
+                //// Web API routes
+                //config.MapHttpAttributeRoutes();
+
+                //config.Routes.MapHttpRoute(
+                //    name: "DefaultApi",
+                //    routeTemplate: "api/{controller}/{id}",
+            } //    defaults: new { id = RouteParameter.Optional }
+
         }
     }
 }
