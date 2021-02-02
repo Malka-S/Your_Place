30a285cd71d456b88a3e675464ab2294aa863989
malkas <malkasvei@gmail.com>
2021-02-02 21:47:04 +0200

    more

---
+++ BLL/UsersService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+   public class UsersService
+    {
+        public static int UpdateUsers(Common.DTO.UsersDto user)
+        {
+            try
+            {
+                return DAL.UsersDal.UpdateUsers(Converters.UsersConverter.ToDalUsers(user));
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+        }
+        public static int AddUsers(Common.DTO.UsersDto user)
+        {
+            try
+            {
+                return DAL.UsersDal.AddUser(Converters.UsersConverter.ToDalUsers(user));
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
+        }
+    }
+}
